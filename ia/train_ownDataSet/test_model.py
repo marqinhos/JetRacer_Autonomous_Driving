90f2a261fc86517862d14387ab22e32f7255f7ac
Marcos <61009765+marqinhos@users.noreply.github.com>
2023-03-30 13:11:19 +0200

    Branch to update utils (#12)
    * Initital commit
    
    * Add code working
    
    * Fix error with predict model
    I was making the prediction wrong

--- ia/train_ownDataSet/test_model.py
+++ ia/train_ownDataSet/test_model.py
@@ -1,9 +1,9 @@
 import os 
 from ultralytics import YOLO       
 
-model_path = os.path.join('.', 'runs', 'detect', 'train7', 'weights', 'best.pt')
+model_path = os.path.join('.', 'runs', 'Segment', 'train', 'train_200epochs_98img_seg', 'weights', 'best.pt')
+
+img_predict = os.path.join('.', 'data', 'image_7.jpeg')
+
 model = YOLO(model_path)  # load a pretrained YOLOv8n segmentation model
-
-img_path = os.path.join('.', 'data', 'images', 'train', 'im1.jpeg')
-
-model(img_path, save=True, save_txt=True) 
+img = model.predict(source=img_predict, show=True, save=True)
