87a7eed4c15d5044f8ba531ccb5701b246181863
marcos <marcosf12400@gmail.com>
2023-04-25 14:10:10 +0200

    Add process function. Go tested it

--- package_ros/pkg_image_process/src/extract_features.py
+++ package_ros/pkg_image_process/src/extract_features.py
@@ -9,6 +9,11 @@ import torch
 from utils import *
 
 class Features_Detection:
+    """Class to extrac the goal point in the image 
+        Errors:
+            - 1: No detection
+            - 2: 
+    """
 
     def __init__(self) -> "Features_Detection":
 
@@ -27,7 +32,8 @@ class Features_Detection:
 
     def run(self, result: list) -> Point:
         try:
-            return self.only_test(result)
+            # return self.only_test(result)
+            return self.process(result)
         
         except Exception as err:
             num_err = err.args[0]
@@ -65,8 +71,78 @@ class Features_Detection:
             
             raise ValueError(2)
 
+
+    def process(self, result: list) -> Point:
+
+        ##############################################################
+        ##                  Check the detections                    ##
+        ##############################################################
+        dict_detections = {}
+        list_class_num = list(self.dict_keys_detec.values())
+        ## Check if lane right detection
+        try:
+            index = self.__get_index_detection(result, list_class_num[0])
+            dict_detections[list_class_num[0]] = index
+        except: pass
+
+        ## Check if lane left detection
+        try:
+            index = self.__get_index_detection(result, list_class_num[1])
+            dict_detections[list_class_num[1]] = index
+        except: pass
+
+        ## Check if middle line detection
+        try:
+            index = self.__get_index_detection(result, list_class_num[2])
+            dict_detections[list_class_num[2]] = index
+        except: pass
+
+        ## Check if corners detection
+        try:
+            list_index = self.__get_index_detection_corner(result, list_class_num[3])
+            index = self.__get_index_corner_right(list_index, result)
+            dict_detections[list_class_num[3]] = index
+        except: pass
+        
+
+        ##############################################################
+        # 1. Detect RIGHT LINE, MIDDLE LINE
+        ##############################################################
+        if all(detect in [2, 3] for detect in list(dict_detections.keys())):
+            mid_mask = result[0].masks[dict_detections[2]].masks.squeeze()
+            mid_pt = self.__get_centroid_line_mid(mid_mask)
+            corner_mask = result[0].masks[dict_detections[3]].masks.squeeze()
+            corner_pt = self.__get_centroid_line_mid(corner_mask)
+            return mid_pt.middle_2_point(corner_pt)
+            
+        ##############################################################
+        # 2. Detect RIGHT LANE
+        ##############################################################
+        if 0 in list(dict_detections.keys()):
+            lane_r_mask = result[0].masks[dict_detections[0]].masks.squeeze()
+            lane_r_pt = self.__get_centroid_line_mid(lane_r_mask)
+            return lane_r_pt
+        
+        ##############################################################
+        # 3. Detect RIGHT LINE
+        ##############################################################
+        if 3 in list(dict_detections.keys()):
+            corner_mask = result[0].masks[dict_detections[3]].masks.squeeze()
+            corner_pt = self.__get_centroid_line_mid(corner_mask)
+            return corner_pt
+        
+        ##############################################################
+        # 4. Detect MIDDLE LINE
+        ##############################################################
+        if 2 in list(dict_detections.keys()):
+            mid_mask = result[0].masks[dict_detections[2]].masks.squeeze()
+            mid_pt = self.__get_centroid_line_mid(mid_mask)
+            return mid_pt
+
+        else: raise ValueError(1) # No Detections
+
     
-    def __get_index_detection(self, result: list, class_name: str) -> torch.Tensor:
+    def __get_index_detection(self, result: list, class_name: str) -> int:
         tensors_detecs = result[0].boxes.cls
         class_num = self.dict_keys_detec[class_name]
         try:
@@ -74,6 +150,38 @@ class Features_Detection:
             return index
         except:
             raise ValueError(1) # Error 1: No detections
+        
+
+    def __get_index_detection_corner(self, result: list) -> list:
+        tensors_detecs = result[0].boxes.cls
+        class_name = "corner_line"
+        class_num = self.dict_keys_detec[class_name]
+        try:
+            index = [(tensors_detecs == class_num).nonzero(as_tuple=True)[0][i].item() for i in range(len((tensors_detecs == class_num).nonzero(as_tuple=True)[0]))]
+            return index
+        except:
+            raise ValueError(1) # Error 1: No detections
+        
+
+    def __get_index_corner_right(self, index: list, result: list) -> list:
+        try:
+            all_point = []
+            for i in index:
+                mask = result[0].masks[i].masks.squeeze()
+                pt = self.__get_centroid_line_corner(mask)
+                all_point.append(pt)
+            if len(all_point) == 1:
+                return index[0]
+            
+            else:
+                if all_point[0].is_right_than(all_point[1]): return index[0]
+                else: return index[1]
+
+        except:
+            raise ValueError(2)
+
+
+
 
     @staticmethod
     def __get_torch_centroid(mask: torch.Tensor) -> Point:
