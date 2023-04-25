918f9bb5262a6f56108f408f83320c07a29f33b0
marcos <marcosf12400@gmail.com>
2023-04-25 15:04:04 +0200

    All tested and working

--- package_ros/pkg_image_process/src/extract_features.py
+++ package_ros/pkg_image_process/src/extract_features.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import os 
 import torch
+import rospy
 
 # Import utils
 from utils import *
@@ -18,12 +19,12 @@ class Features_Detection:
     def __init__(self) -> "Features_Detection":
 
         ########################### IMAGE ###########################
-        self.offset_right = 160 + 20 #px
-        self.offset_mid = 160 + 20 #px
+        self.offset_right = 160 + 20 + 10 #px
+        self.offset_mid = 160 + 20 + 40 +10 #px
         self.size = [640, 480] # width, height
 
         ########################### DETECTIONS ###########################
-        self.dict_keys_detec = {
+        self.dict_keys_detect = {
                                 "right_lane": 0,
                                 "left_lane": 1,
                                 "middle_line": 2,
@@ -32,7 +33,6 @@ class Features_Detection:
 
     def run(self, result: list) -> Point:
         try:
-            # return self.only_test(result)
             return self.process(result)
         
         except Exception as err:
@@ -45,32 +45,6 @@ class Features_Detection:
                 ## Other error
                 raise ValueError(2)
 
-            """
-            num_err = err.args[0]
-            if num_err == 1:
-                ## No detections
-                return Point(self.size[0]//2, self.size[1])
-            elif num_err == 2:
-                ## Other error
-                return Point(self.size[0]//2, self.size[1])
-            """
-    
-    def only_test(self, result: list) -> Point:
-        class_name = "right_lane"
-        try:
-            index_mask = self.__get_index_detection(result, class_name)
-            mask = result[0].masks[index_mask].masks.squeeze()
-            pt_centroid = self.__get_centroid_lane(mask)
-            return pt_centroid
-        
-        except Exception as err:
-            num_err = err.args[0]
-            if num_err == 1:
-                ## No detections 
-                raise ValueError(1)
-            
-            raise ValueError(2)
-
 
     def process(self, result: list) -> Point:
 
@@ -78,63 +52,66 @@ class Features_Detection:
         ##                  Check the detections                    ##
         ##############################################################
         dict_detections = {}
-        list_class_num = list(self.dict_keys_detec.values())
+        list_class_num = list(self.dict_keys_detect.values())
         ## Check if lane right detection
         try:
-            index = self.__get_index_detection(result, list_class_num[0])
+            index = self.__get_index_detection(result, class_num=list_class_num[0])
             dict_detections[list_class_num[0]] = index
         except: pass
 
         ## Check if lane left detection
         try:
-            index = self.__get_index_detection(result, list_class_num[1])
+            index = self.__get_index_detection(result, class_num=list_class_num[1])
             dict_detections[list_class_num[1]] = index
         except: pass
 
         ## Check if middle line detection
         try:
-            index = self.__get_index_detection(result, list_class_num[2])
+            index = self.__get_index_detection(result, class_num=list_class_num[2])
             dict_detections[list_class_num[2]] = index
         except: pass
 
         ## Check if corners detection
         try:
-            list_index = self.__get_index_detection_corner(result, list_class_num[3])
+            list_index = self.__get_index_detection_corner(result, class_num=list_class_num[3])
             index = self.__get_index_corner_right(list_index, result)
             dict_detections[list_class_num[3]] = index
         except: pass
         
-
         ##############################################################
         # 1. Detect RIGHT LINE, MIDDLE LINE
-        ##############################################################
-        if all(detect in [2, 3] for detect in list(dict_detections.keys())):
+        ############################################################## 
+        if all(detect in list(dict_detections.keys()) for detect in [2, 3]):
+            rospy.loginfo("RIGHT CORNER -- MIDDLE LINE")
             mid_mask = result[0].masks[dict_detections[2]].masks.squeeze()
-            mid_pt = self.__get_centroid_line_mid(mid_mask)
+            mid_pt = self.__get_torch_centroid(mid_mask)
             corner_mask = result[0].masks[dict_detections[3]].masks.squeeze()
-            corner_pt = self.__get_centroid_line_mid(corner_mask)
+            corner_pt = self.__get_torch_centroid(corner_mask)
             return mid_pt.middle_2_point(corner_pt)
             
         ##############################################################
         # 2. Detect RIGHT LANE
         ##############################################################
         if 0 in list(dict_detections.keys()):
+            rospy.loginfo("RIGHT LANE")
             lane_r_mask = result[0].masks[dict_detections[0]].masks.squeeze()
-            lane_r_pt = self.__get_centroid_line_mid(lane_r_mask)
+            lane_r_pt = self.__get_centroid_lane(lane_r_mask)
             return lane_r_pt
         
         ##############################################################
         # 3. Detect RIGHT LINE
         ##############################################################
         if 3 in list(dict_detections.keys()):
+            rospy.loginfo("RIGHT CORNER")
             corner_mask = result[0].masks[dict_detections[3]].masks.squeeze()
-            corner_pt = self.__get_centroid_line_mid(corner_mask)
+            corner_pt = self.__get_centroid_line_corner(corner_mask)
             return corner_pt
         
         ##############################################################
         # 4. Detect MIDDLE LINE
         ##############################################################
         if 2 in list(dict_detections.keys()):
+            rospy.loginfo("MIDDLE LINE")
             mid_mask = result[0].masks[dict_detections[2]].masks.squeeze()
             mid_pt = self.__get_centroid_line_mid(mid_mask)
             return mid_pt
@@ -142,20 +119,25 @@ class Features_Detection:
         else: raise ValueError(1) # No Detections
 
     
-    def __get_index_detection(self, result: list, class_name: str) -> int:
+    def __get_index_detection(self, result: list, class_name: str=None, class_num: int=None) -> int:
         tensors_detecs = result[0].boxes.cls
-        class_num = self.dict_keys_detec[class_name]
+        if class_num is None:
+            class_num = self.dict_keys_detect[class_name]
+        else:
+            class_num = class_num
         try:
-            index = (tensors_detecs == class_num).nonzero(as_tuple=True)[0]
+            index = (tensors_detecs == class_num).nonzero(as_tuple=True)[0].item()
             return index
         except:
             raise ValueError(1) # Error 1: No detections
         
 
-    def __get_index_detection_corner(self, result: list) -> list:
+    def __get_index_detection_corner(self, result: list, class_name: str=None, class_num: int=None) -> list:
         tensors_detecs = result[0].boxes.cls
-        class_name = "corner_line"
-        class_num = self.dict_keys_detec[class_name]
+        if class_num is None:
+            class_num = self.dict_keys_detect[class_name]
+        else:
+            class_num = class_num
         try:
             index = [(tensors_detecs == class_num).nonzero(as_tuple=True)[0][i].item() for i in range(len((tensors_detecs == class_num).nonzero(as_tuple=True)[0]))]
             return index
@@ -179,8 +161,6 @@ class Features_Detection:
 
         except:
             raise ValueError(2)
-
-
 
 
     @staticmethod
@@ -227,7 +207,7 @@ class Features_Detection:
 
 
     def __get_centroid_lane(self, lane_mask: torch.Tensor) -> Point:
-        centroid_pt = self.__get_numpy_centroid(lane_mask)
+        centroid_pt = self.__get_torch_centroid(lane_mask)
         return centroid_pt
 
 
