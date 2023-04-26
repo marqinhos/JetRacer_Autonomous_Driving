906b5346b425501e925c0db3bf803f28290bde16
marcos <marcosf12400@gmail.com>
2023-04-26 15:50:58 +0200

    Add more ranges to check

--- package_ros/pkg_rplidar_publisher/src/detect_obstacles.py
+++ package_ros/pkg_rplidar_publisher/src/detect_obstacles.py
@@ -3,20 +3,22 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
+import numpy as np
 
 def scan_callback(msg):
-    # Angle to start
-    start_angle = msg.angle_min
-    end_angle = -0.5
-    # Index to start
-    start_index = int((start_angle - msg.angle_min) / msg.angle_increment)
-    end_index = int((end_angle - msg.angle_min) / msg.angle_increment)
     
-    # Calcular el promedio de los valores en ese rango de ángulos
-    avg_value = sum(msg.ranges[start_index:end_index]) / (end_index - start_index)
-    # Detectar obstáculos en el lado derecho
-    if avg_value < 1.0:
-        rospy.loginfo("Obstacle detected on the right side")
+    MIN_CENTER = .16
+    MIN_BACK = .18
+    MIN_SIDES = .17
+
+
+    laser_beam_c = msg.ranges[0:80] + msg.ranges[len(msg.ranges)-80:len(msg.ranges)]
+    laser_beam_l = msg.ranges[80:280]
+    laser_beam_b = msg.ranges[280:440]
+    laser_beam_r = msg.ranges[440:640]
+
+    if min(laser_beam_c) <= MIN_CENTER or min(laser_beam_b) <= MIN_BACK or min(laser_beam_r+laser_beam_l) <= MIN_SIDES:
+        rospy.loginfo("Obstacle detected AUTOMATIC STOP")
         obstacle_pub.publish(True)
     else:
         obstacle_pub.publish(False)
@@ -24,5 +26,5 @@ def scan_callback(msg):
 if __name__ == '__main__':
     rospy.init_node('obstacle_detector')
     scan_sub = rospy.Subscriber('/scan', LaserScan, scan_callback)
-    obstacle_pub = rospy.Publisher('/obstacle_detected', Bool, queue_size=1)
+    obstacle_pub = rospy.Publisher('/jetracer_obstacle_detected', Bool, queue_size=1)
     rospy.spin()
